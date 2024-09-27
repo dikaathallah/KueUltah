07ad195cb2f4bbe02d5018f83bc1dd764981af8f
dikaathallah <78903750+dikaathallah@users.noreply.github.com>
2024-09-27 13:21:52 +0700

    Update jquery.playSound.js

--- asset/jquery.playSound.js
+++ asset/jquery.playSound.js
@@ -1,6 +1,4 @@
 /**
- * @author Alexander Manzyuk <admsev@gmail.com>
- * Copyright (c) 2012 Alexander Manzyuk - released under MIT License
  * https://github.com/admsev/jquery-play-sound
  * Usage: $.playSound('http://example.org/sound')
  * $.playSound('http://example.org/sound.wav')
